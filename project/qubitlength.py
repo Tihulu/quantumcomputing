4a038ea86fd86fbe60d0a0d9f1108f6a1332f348
Tihulu <40742160+Tihulu@users.noreply.github.com>
2022-01-25 03:49:17 +0300

    Update qubitlength.py

--- project/qubitlength.py
+++ project/qubitlength.py
@@ -1,12 +1,11 @@
 text=input('N = p*q, type N or bitlength of rsa:\n 1 for N, 2 for bitl. (format: 1, N):')
 txt=text.split(',')
 select , val = txt
-
-if select=='1':
+val=int(val)
+if(select=='1'):
 	n=math.ceil(math.log2(val))
 	bitn=2*n+3
 	print(bitn,' qubits are needed')
-if select=='2':
+if(select=='2'):
 	bitn=2*val+3
 	print(bitn,' qubits are needed')
-x=input('')
