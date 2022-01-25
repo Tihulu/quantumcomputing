ef08146d9c72a28ffc673f458c56434bf3f9cf66
Tihulu <40742160+Tihulu@users.noreply.github.com>
2022-01-25 06:30:32 +0300

    Update rsaqattack.py

--- project/rsaqattack.py
+++ project/rsaqattack.py
@@ -513,8 +513,11 @@ if __name__ == '__main__':
 
     """ Simulate the created Quantum Circuit """
     aaat=input('press 1 to draw 2  for not to:')
+    while True:
     if aaat=='1':
         circuit.draw(output='text')
+        break
+    else:
         break
     aaat=input('press to continue:')
     provider = IBMQ.load_account()
